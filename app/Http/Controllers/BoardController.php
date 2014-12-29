e25811523dabed85d8102dd763f4c51f8c2fbbce
jifei <1045195056@qq.com>
2014-12-29 16:49:00 +0800

    +board

---
+++ app/Http/Controllers/BoardController.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: jifei
+ * Date: 14/12/29
+ * Time: 下午4:09
+ */
+
+namespace App\Http\Controllers;
+
+
+class BoardController extends Controller
+{
+
+    public function add()
+    {
+        $data = [['id'=>'1','name'=>'','code'=>'','pid']];
+    }
+} 
