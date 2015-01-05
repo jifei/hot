fd80dc6b7d7785436344bc983e1062148b52871e
jifei <123456>
2015-01-06 00:14:12 +0800

    @navbar

---
+++ app/Http/Composer/NavbarComposer.php
@@ -0,0 +1,25 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: jifei
+ * Date: 15/1/5
+ * Time: 22:56
+ */
+namespace App\Http\Composer;
+
+use App\Repositories\Board\BoardRepository;
+use Illuminate\Contracts\View\View;
+
+class NavbarComposer
+{
+    public function __construct()
+    {
+        $this->board = new BoardRepository();
+    }
+
+    public function compose(View $view)
+    {
+        $view->with('top_boards',$this->board->getTopBoards());
+    }
+}
---
+++ resources/templates/includes/navbar.blade.php
@@ -0,0 +1,64 @@
+<div class="navbar navbar-inverse">
+    <div class="navbar-inner">
+        <div class="container">
+            <a class="btn btn-navbar" data-toggle="collapse" data-target=".navbar-responsive-collapse">
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </a>
+            <a class="brand" href="http://www.redudian.com" >热度点</a>
+            <div class="nav-collapse collapse navbar-responsive-collapse">
+                <ul class="nav">
+                    <li @if(0=='')class="active"@endif><a href="/">首页</a></li>
+                    @foreach($top_boards as $v)
+                       <li><a href="/f/{{$v['code']}}">{{$v['name']}}</a></li>
+                    @endforeach
+                    <li class="dropdown">
+                        <a href="#" class="dropdown-toggle" data-toggle="dropdown">版块<b class="caret"></b></a>
+                        <ul class="dropdown-menu">
+                            <li><a href="#">新闻</a></li>
+                            <li><a href="#">自媒体</a></li>
+                            <li><a href="#">搞笑</a></li>
+                            <li><a href="#">财经</a></li>
+                            <li><a href="#">娱乐</a></li>
+                            <li><a href="#">电视</a></li>
+                            <li><a href="#">电影</a></li>
+                            <li><a href="#">体育</a></li>
+                            <li><a href="#">科技</a></li>
+                            <li><a href="#">汽车</a></li>
+                            <li><a href="#">房产</a></li>
+                            <li><a href="#">社会</a></li>
+                            <li><a href="#">公益</a></li>
+                            <li><a href="#">问答</a></li>
+<!--                            <li><a href="#">情感</a></li>-->
+<!--                            <li><a href="#">美食</a></li>-->
+<!--                            <li><a href="#">旅游</a></li>-->
+<!--                            <li><a href="#">读书</a></li>-->
+                            <li class="divider"></li>
+                            <li><a href="#">更多>></a></li>
+                        </ul>
+                    </li>
+                    <li><a href="/more">更多>></a></li>
+                </ul>
+                <form class="navbar-search pull-left" action="">
+                    <input  type="text" class="search-query" maxlength="15" placeholder="搜索">
+                    <i class="icon-search"></i>
+                </form>
+                <ul class="nav pull-right">
+                    <li><button style="margin-right:5px;" type="submit" href="#publishModal"  data-toggle="modal" class="btn btn-warning publish-btn">发布</button></li>
+                    <li><a href="#loginModal"  data-toggle="modal"  class="login">登录</a></li>
+                    <li><a href="#registerModal"  data-toggle="modal"  class="register">注册</a></li>
+<!--                    <li class="divider-vertical"></li>-->
+<!--                    <li class="dropdown">-->
+<!--                        <a href="#" class="dropdown-toggle" data-toggle="dropdown">Dropdown <b class="caret"></b></a>-->
+<!--                        <ul class="dropdown-menu">-->
+<!--                            <li><a href="#">个人信息</a></li>-->
+<!--                            <li class="divider"></li>-->
+<!--                            <li><a href="#">设置</a></li>-->
+<!--                        </ul>-->
+<!--                    </li>-->
+                </ul>
+            </div><!-- /.nav-collapse -->
+        </div>
+    </div><!-- /navbar-inner -->
+</div>
