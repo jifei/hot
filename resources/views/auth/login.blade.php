8f3ec74c6e6ac0d35164e1e6b9d4ae50e7a30408
jifei <123456>
2015-01-08 21:10:31 +0800

    @change templates to views

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -2,5 +2,6 @@
    <input name="email">
    {{--<input type="hidden" name="_token" value="{{{ csrf_token() }}}" />--}}
    <input name="password">
+   <input name="remember" value="1" type="checkbox">
    <input type="submit">
 </form>
